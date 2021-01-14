bce2e66f07edb1416053006473a21ce760dd8f7c
mhans003 <michaeledwardhanson@gmail.com>
2021-01-14 14:13:27 -0800

    When assigning a new book object, check to see if those properties exist, otherwise assign null. This prevents issue of the app crashing when there is no image array existing and one tries to add a book; Added a modal to show that a book is already added which will later be used once apiID key is unique.

---
+++ client/src/components/AddErrorModal/index.js
@@ -0,0 +1,31 @@
+import { Modal } from "react-bootstrap";
+
+const AddErrorModal = (props) => {
+
+    return (
+        <>
+            <Modal show={props.addErrorShow} onHide={props.handleAddErrorClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        <h2 className="font-medium">Added</h2>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="show-grid">
+                    <div>
+                        <h3 className="font-light">This book is already saved in your library</h3>
+                        <hr/>
+                        <p><strong>{props.title}</strong></p>
+                        <button className="btn btn-outline-primary btn-lg btn-block mt-3 mb-3" type="button" onClick={() => {
+                            props.handleAddErrorClose();
+                        }}>
+                            <i className="fas fa-check"></i>
+                        </button>
+                        <hr className="mb-4"/>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+}
+
+export default AddErrorModal;
--- client/src/components/SearchResult/index.js
+++ client/src/components/SearchResult/index.js
@@ -4,6 +4,7 @@ import BookCard from "../BookCard";
 import BookCardBody from "../BookCardBody";
 import BookCardHeader from "../BookCardHeader";
 import ConfirmAddModal from "../ConfirmAddModal";
+import AddErrorModal from "../AddErrorModal";
 
 //Include API functions.
 import APIService from "../../Services/APIService";
@@ -11,18 +12,19 @@ import APIService from "../../Services/APIService";
 const SearchResult = (props) => {
 
     const [addBookShow, setAddBookShow] = useState(false);
+    const [addErrorShow, setAddErrorShow] = useState(false);
 
     const handleFavoriteSave = event => {
         const newBook = {
-            title: props.result.volumeInfo.title,
-            authors: props.result.volumeInfo.authors,
-            description: props.result.volumeInfo.description,
-            image: props.result.volumeInfo.imageLinks.thumbnail,
-            thumbnail: props.result.volumeInfo.imageLinks.smallThumbnail,
-            link: props.result.volumeInfo.infoLink,
+            title: props.result.volumeInfo.title ? props.result.volumeInfo.title : null,
+            authors: props.result.volumeInfo.authors ? props.result.volumeInfo.authors: null,
+            description: props.result.volumeInfo.description ? props.result.volumeInfo.description : null,
+            image: props.result.volumeInfo.imageLinks ? props.result.volumeInfo.imageLinks.thumbnail : null,
+            thumbnail: props.result.volumeInfo.imageLinks ? props.result.volumeInfo.imageLinks.smallThumbnail : null,
+            link: props.result.volumeInfo.infoLink ? props.result.volumeInfo.infoLink : null,
             read: false,
             apiID: props.result.id,
-            categories: props.result.volumeInfo.categories
+            categories: props.result.volumeInfo.categories ? props.result.volumeInfo.categories : null
         }
 
         APIService.saveBook(newBook).then(data => {
@@ -30,6 +32,7 @@ const SearchResult = (props) => {
             handleAddBookShow();
         })
         .catch(error => {
+            console.log(error);
             if(error) {
                 console.log("not authenticated");
                 console.log("Error. Not authorized. Save to global state");
@@ -51,6 +54,9 @@ const SearchResult = (props) => {
     const handleAddBookClose = () => setAddBookShow(false);
     const handleAddBookShow = () => setAddBookShow(true);
 
+    const handleAddErrorClose = () => setAddErrorShow(false);
+    const handleAddErrorShow = () => setAddErrorShow(true);
+
     return (
         <>
             <BookCard>
@@ -69,6 +75,11 @@ const SearchResult = (props) => {
                 title={props.result.volumeInfo.title}
                 addBookShow={addBookShow}
                 handleAddBookClose={handleAddBookClose}
+            />
+            <AddErrorModal
+                title={props.result.volumeInfo.title}
+                addErrorShow={addErrorShow}
+                handleAddErrorClose={handleAddErrorClose}
             />
         </>
     );
